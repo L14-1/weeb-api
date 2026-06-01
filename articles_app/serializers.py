591bbdd0afb247d29f7330bfd540c2b3d7f94841
baku67 <basile08@hotmail.fr>
2026-06-01 19:09:08 +0200

    FEATURE: add CRUD articles

---
+++ articles_app/serializers.py
@@ -0,0 +1,38 @@
+from rest_framework import serializers
+from auth_app.serializers import UserSerializer
+from .models import Article
+
+
+class ArticleSerializer(serializers.ModelSerializer):
+    owner = UserSerializer(read_only=True)
+
+    class Meta:
+        model = Article
+        fields = (
+            'id',
+            'title',
+            'content',
+            'owner',
+            'created_at',
+            'updated_at',
+        )
+        read_only_fields = (
+            'id',
+            'owner',
+            'created_at',
+            'updated_at',
+        )
+
+
+    def validate_title(self, value):
+        value = value.strip()
+        if not value:
+            raise serializers.ValidationError("Le titre est requis")
+        return value
+
+    def validate_content(self, value):
+        value = value.strip()
+        if not value:
+            raise serializers.ValidationError("Le contenu est requis")
+
+        return value
---
+++ articles_app/views.py
@@ -0,0 +1,16 @@
+from rest_framework import viewsets
+from .models import Article
+from .permissions import IsOwnerOrAdminOrReadOnly
+from .serializers import ArticleSerializer
+
+"""
+CRUD complet pour les articles
+"""
+class ArticleViewSet(viewsets.ModelViewSet):
+    queryset = Article.objects.select_related('owner', 'owner__profile').all()
+    serializer_class = ArticleSerializer
+    permission_classes = [IsOwnerOrAdminOrReadOnly]
+    http_method_names = ['get', 'post', 'put', 'patch', 'delete', 'head', 'options']
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
