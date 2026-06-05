c799b0b8f12dbf5ef3235fa28ecf9b3a28cf2599
Nicolas Maitre <nico.maitre.stair@gmail.com>
2026-06-05 11:22:06 +0200

    FIX: Dockerfile & environment production file

--- core/settings/production.py
+++ core/settings/production.py
@@ -1,10 +1,13 @@
 import os
 from .base import *
 
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY', 'k)4*lj1#tdb__9)mdma#u98^@=o=whju5b@lh6fxxq+@*f_hj4')
 DEBUG = False
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',')
 CSRF_TRUSTED_ORIGINS = os.getenv('CSRF_TRUSTED_ORIGINS', '').split(',')
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STORAGES = {
+    'default': {'BACKEND': 'django.core.files.storage.FileSystemStorage'},
+    'staticfiles': {'BACKEND': 'whitenoise.storage.CompressedManifestStaticFilesStorage'},
+}
